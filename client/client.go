fc074b3baf7d34019587be4f6029bb8a2c24f877
熠丶 <490493499@qq.com>
2022-10-06 16:23:17 +0800

    客户端实现-建立连接

---
+++ client/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	conn       net.Conn
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
+	if err != nil {
+		fmt.Println("net.Dial error:", err)
+		return nil
+	}
+	client.conn = conn
+	return client
+}
